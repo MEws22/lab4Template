70a1467ac5d72a5c6a7f3fdf60d84e5c3da7749b
Simone Andreetto <simone.andreetto@gmail.com>
2022-12-14 10:56:47 +0100

    add expected src-gen files

---
+++ cpsml.codegen/src-gen-expected/component/Extrudor.java
@@ -0,0 +1,39 @@
+package production.component;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class Extrudor{
+	
+	private String name;
+	private int runs;
+	private int ratedRuns;
+	private String topic;
+	
+	public Extrudor(String name) {
+		this.name = name;
+		this.runs = 0;
+		this.ratedRuns = 100;
+		this.topic = "production";
+	}
+	
+	public void startPrint(){
+		System.out.println("    🔵  Actuator called function execution: " + this.name + " -> startPrint() returning type NULL");
+		return ;
+	} 
+	public int getRemainingPrintDuration(){
+		System.out.println("    🔵  Actuator called function execution: " + this.name + " -> getRemainingPrintDuration() returning type INT");
+		return 2076191646;
+	} 
+
+	public String getName() {
+		return name;
+	}
+	
+	public String getTopic(){
+		return this.topic;
+	}
+	
+	public void step(){
+	}
+}
---
+++ cpsml.codegen/src-gen-expected/component/InternalTemperature.java
@@ -0,0 +1,42 @@
+package production.component;
+
+import java.util.List;
+
+public class InternalTemperature {
+	
+	private String name;
+	private double frequency;
+	private String topic;
+	private int internalStepCount;
+	
+	public InternalTemperature(String name) {
+		this.name = name;
+		this.frequency = 0.25;
+		this.topic = "temp";
+		this.internalStepCount = 0;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getTopic(){
+		return this.topic;
+	}
+	
+	public int getTemperature(){
+		System.out.println("    🟣  Sensor timed function execution: " + this.name + " -> getTemperature() returning type INT");
+		return 291155745;
+	}
+	
+	public void addMessageToNodeOutgoingCache(String topic, String msgContent, String recipient, List<String> msgOutCache) {
+		msgOutCache.add(topic + "-" + recipient + "-" + msgContent);
+	}
+	
+	public void step(List<String> msgOutCache) {
+		if (this.internalStepCount % (1/frequency) == 0 ) {
+			addMessageToNodeOutgoingCache(this.topic, "" + this.getTemperature(), "NONE" , msgOutCache);
+		}
+		this.internalStepCount++;
+	}
+}
